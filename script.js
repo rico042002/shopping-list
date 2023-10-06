a8374cf2cbdff6d115098ed345081e767c04f5e4
Rico042002 <rico042002@gmail.com>
2023-10-05 23:04:59 -0500

    filter list

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearButton = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 const addItem = (e) => {
   e.preventDefault();
@@ -16,7 +18,9 @@ const addItem = (e) => {
 
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
+  //Add li to the DOM
   itemList.appendChild(li);
+  checkUI();
   itemInput.value = '';
 };
 
@@ -34,5 +38,56 @@ const createIcon = (classes) => {
   return icon;
 };
 
+function removeItem(e) {
+  //clicking on icon whos parent is a button with a class of "remove-item"
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+}
+
+const clearItems = () => {
+  // itemList.innerHTML = ''; //optimized way below
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+};
+
+function filterItems(e) {
+  //capture text and make lowercase
+  const text = e.target.value.toLowerCase();
+  const items = itemList.querySelectorAll('li');
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+  console.log(text);
+}
+
+function checkUI() {
+  //Needs to be inside function to check when list items are added
+  const items = itemList.querySelectorAll('li');
+  if (items.length === 0) {
+    clearButton.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearButton.style.display = 'block';
+    itemFilter.style.display = 'block';
+  }
+}
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearButton.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+checkUI();
